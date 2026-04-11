3518db5bbdda11abaffe92d04c2508f803860e2f
chivi0801 <luuchivylp76@gmail.com>
2026-04-11 20:55:23 +0700

    tạo item mỗi dòng bài làm thí sinh

--- Android/app/src/main/java/com/example/android_python/ExamAdapter.java
+++ Android/app/src/main/java/com/example/android_python/ExamAdapter.java
@@ -27,7 +27,7 @@ public class ExamAdapter extends RecyclerView.Adapter<ExamAdapter.ExamViewHolder
     @NonNull
     @Override
     public ExamViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_exam, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_kythi, parent, false);
         return new ExamViewHolder(view);
     }
 
